b23b27416b7ccc3c8a77370cd0e1aba06ae2be04
KemiyaN01 <66344081+KemiyaN01@users.noreply.github.com>
2021-11-09 11:11:33 +0900

    Add files via upload
    ダークモード・ライトモード切り替えをわかりやすくした。
    スクロールメニューも追加した。

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
+//ダークモード切り替え用変数。
 let darkmode = 1;
-var dark1 = document.getElementById("dark");
+var dark1 = document.getElementById("i_dark");
 var moonbtn = document.getElementById("Mwhite");
 //初めにサイトに入ってきた人がダークモードかどうかを確認してテーマを実行している。//
 if (window.matchMedia('(prefers-color-scheme: dark)').matches === true) {
-    console.log("it is dark");
     document.documentElement.setAttribute('theme', 'dark');
     darkmode = 1;
 } else {
-    console.log("it's not dark");
     document.documentElement.setAttribute('theme', 'light');
     darkmode = 0;
 }
-
+console.log(darkmode);
+//月のクリック
 moonbtn.onclick = function() {
-    ClickChangeDarkmode();
-}
-
+        ClickChangeDarkmode();
+    }
+    //ダークモード
 function ChangeDarakmode() {
     document.documentElement.setAttribute('theme', 'dark');
 }
-
+//ライトモード
 function ChangeLightmode() {
     document.documentElement.setAttribute('theme', 'light');
 }
-
+//ダークモード・ライトモードのアニメーションを開始している
 function ClickChangeDarkmode() {
-    if (darkmode == 1) {
-        window.setTimeout(ChangeDarakmode, 1400);
-        darkmode = 0;
-        dark1.style.animation = "darkwavestart 2s ease 0s 1 normal forwards running";
+    if (darkmode == 0) {
+        window.setTimeout(ChangeDarakmode, 700);
+        darkmode = 1;
+        dark1.style.animation = "darkwavestart 1s ease 0s 1 normal forwards running";
     } else {
         ChangeLightmode();
-        darkmode = 1;
-        dark1.style.animation = "darkwaveend 2s ease 0s 1 normal forwards running";
+        darkmode = 0;
+        dark1.style.animation = "darkwaveend 1s ease 0s 1 normal forwards running";
     }
 }
